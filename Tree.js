f696b67dc706e40db1f7967992077c34ae7b3511
Harshitjainpro <73413709+Harshitjainpro@users.noreply.github.com>
2021-01-05 22:08:39 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,11 @@
+class Tree {
+    constructor(x,y,width,hight){
+        var option = {
+            isStatic: true,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+        
+    }
+}
